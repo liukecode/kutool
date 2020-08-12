9778da814284ed0d2f9074e482011b1687c017ff
ke <821711401@qq.com>
2020-08-13 01:42:49 +0800

    classifiers

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ketool",  # Replace with your own username
-    version="0.0.3",
+    name="kutool",  # Replace with your own username
+    version="0.0.1",
     author="Liuke",
-    author_email="",
-    keywords=['python', 'KeTool'],
+    author_email="821711401@qq.com",
+    keywords=['python', 'KuTool'],
     description="A set of tools that make coding more simpler for python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,7 +17,7 @@ setuptools.setup(
     platforms=["all"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3',
